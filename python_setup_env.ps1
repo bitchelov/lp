37e26b3ff2ddbc2cd523b07618592bd7cc4835ec
bitchelov <sokurito@gmail.com>
2018-07-05 23:22:53 +0300

    Add pytest conf. file, virtual_env ps script, requirements.txt

---
+++ python_setup_env.ps1
@@ -0,0 +1,8 @@
+﻿Set-Location $PSScriptRoot
+Invoke-Command {python -m venv dev_env}
+Set-Location -Path "dev_env\Scripts"
+.\activate
+$updatepip = Start-Job { python -m pip install --upgrade pip }
+Wait-Job $updatepip
+Set-Location $PSScriptRoot
+pip install -r requirements.txt
